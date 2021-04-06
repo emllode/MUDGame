5cdb56728bb11c4d00f98ed7116b86af8b71f25e
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-04-06 16:36:47 +0200

    some stuff.

--- main.go
+++ main.go
@@ -1,30 +1,37 @@
 package main
 
-import {
+import (
 	"log"
 	"net"
-}
+)
 
-func handleConnection(conn *.net.Conn) error {
-
+func handleConnection(conn net.Conn) error {
+	log.Println("I got an connection!")
+	return nil
 }
 
 func startServer() error {
 	log.Println("Starting Server")
 
-	ln. err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		return err
 	}
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Printf("Error accepting connection", err)
+			log.Println("Error accepting connection", err)
 			continue
 		}
-		go handleConnection(conn)
+		go func() {
+			if err := handleConnection(conn); err != nil {
+				log.Println("Error handling  connection", err)
+				return
+			}
+		}()
+
 	}
-	return nil
+
 }
 
 func main() {
